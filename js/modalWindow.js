dee822d5e3a2c31d8dd1f9fe6a78ff7354e25f11
Maks <an-tar70@ya.ru>
2022-05-03 14:46:10 +0700

    added modal window for showing images

---
+++ js/modalWindow.js
@@ -0,0 +1,33 @@
+/**____________________________MODAL WINDOW(photo)_______________________________ */
+const photo = Array.prototype.slice.call(document.querySelectorAll('.works__container > div'));
+const modal = document.getElementById('myModal');
+const span = document.querySelector('.close');
+const modalEl = document.querySelector('.modal-Content>img');
+
+for (const i in photo) {
+
+   photo[i].addEventListener('click', event => {
+      let target = event.target;
+      if (target.className != 'works__img') return;
+      modalEl.setAttribute('src', `img/${i}.jpg`);
+      modal.style.display = 'block';
+      span.classList.toggle('_active');
+      body.classList.toggle('_lock');
+   })
+
+}
+
+span.onclick = function () {
+   modal.style.display = 'none';
+   span.classList.remove('_active');
+   body.classList.remove('_lock');
+}
+
+window.onclick = function (event) {
+   if (event.target == modal) {
+      modal.style.display = 'none';
+      span.classList.remove('_active');
+      body.classList.remove('_lock');
+   }
+}
+/**____________________________MODAL WINDOW(photo)_______________________________ */
